8cec2c89363beeb6f4e66a37bdf6f7f7a3ef79a4
yonghui.cui <cuiyonghui@tuandai.com>
2017-09-05 17:45:03 +0800

    1

--- autotest/src/main/java/cn/tdw/common/constants/TaskInfoEnum.java
+++
@@ -1,8 +0,0 @@
-package cn.tdw.common.constants;
-
-/**
- * Created by wuganqin on 2017/8/23.
- */
-public enum TaskInfoEnum {
-    TOTAL,MONTHLY,DAILY
-}
--- autotest/src/main/resources/static/js/collection/cellColor.js
+++
@@ -1,7 +0,0 @@
-function genStatusTextColor(cellvalue, options, rowObject) {
-    if (cellvalue) {
-        return cellvalue + "(<span style='color: red'>" + rowObject.overdueDays + "</span>)";
-    } else {
-        return '';
-    }
-}
